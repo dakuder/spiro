5a8431da3b6f7f876e88d5e40691399c60ab9c9a
dkuder <d@localhost>
2015-01-18 13:25:59 -0800

    More parameters

--- perl-svg.pl
+++ perl-svg.pl
@@ -4,7 +4,7 @@ use Modern::Perl;
 use SVG;
 use Math::Trig ':pi';
 
-my $R = 100;
+my $R = 128;
 my $r;
 my $p;
 my $phi;
@@ -14,24 +14,26 @@ my $A;
 my $step;
 my $pixels = '512';
 my $half = '256';
+my $stroke_width;
 
 # Hardcoded parameters
 $N=5;
 $M=1;
 $r = ($M / $N)*$R;
-$p = $r;
-$phi = 0;
-$A = 200;
+$p = 2.2 * $r;
+$phi = pi/20;
+$A = 400;
 $step=(2 * $N * pi)/$A;
 my $sigma=($R-$r)/$r;
+$stroke_width = 5;
 
 # loop 
 my @xs;
 my @ys;
 
 for(my $t=0; $t<(2 * $N * pi); $t = $t + $step) {
-	my $x = ($R - $r) * cos($t) + $r * cos($sigma * $t);
-	my $y = ($R - $r) * sin($t) - $r * sin($sigma * $t);
+	my $x = ($R - $r) * cos($t) + $p * cos($sigma * $t + $phi);
+	my $y = ($R - $r) * sin($t) - $p * sin($sigma * $t + $phi);
 	push @xs, $x;
 	push @ys, $y;
 }
@@ -41,6 +43,6 @@ my $image = $svg->group(id=>'image');
 
 # convert array of points to svg polygon
 my $points = $image->get_path(x=>\@xs, y=> \@ys, -type=>'polygon');
-my $c = $image->polygon(transform=>"translate(256, 256)", style=>"stroke: black; fill: none;", %$points ); 
+my $c = $image->polygon(transform=>"translate(256, 256)", style=>"stroke: black; fill: none; stroke-width: $stroke_width", %$points ); 
 
 print $svg->xmlify;
