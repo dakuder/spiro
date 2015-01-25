c11077bd7b8d55c62a50032c9178213ea559b7de
dkuder <d@localhost>
2015-01-25 09:07:09 -0800

    added GCD, "all" params

--- perl-svg.pl
+++ perl-svg.pl
@@ -4,12 +4,24 @@ use Modern::Perl;
 use SVG;
 use Math::Trig ':pi';
 
+sub gcd {
+   my $t;
+   my $a = shift;
+   my $b = shift;
+   while ($b > 0) {
+     $t= $b;
+     $b = $a % $b;
+     $a = $t;
+   };
+   return $a;
+};
+
 my $R = 128;
 my $r;
 my $p;
 my $phi;
-my $N;
-my $M;
+my $NN;
+my $MM;
 my $A;
 my $step;
 my $pixels = '512';
@@ -17,27 +29,37 @@ my $half = '256';
 my $stroke_width;
 my $pen_color;
 my $fill_color;
+my $scale_x;
+my $scale_y;
+my $theta;
+my $offset_x;
+my $offset_y;
 
 # Hardcoded parameters
-$N=5;
-$M=2;
+$NN=11;
+$MM=5;
+# assuming N>M and N/2 > M;
+my $f = gcd($NN, $MM);
+my $N = $NN / $f;
+my $M = $MM / $f;
 $r = ($M / $N)*$R;
-$p = 2.2 * $r;
+$p = 1 * $r;
 $phi = pi/20;
-$A = 50;
+$A = 150;
 $step=(2 * pi)/$A;
 my $sigma=($R-$r)/$r;
-$stroke_width = 3;
-$pen_color = "rgb(255, 100,100)";
+$stroke_width = 1;
+$pen_color = "rgb(0, 0,0)";
+$fill_color = "rgb(80,240,100)";
 
 # create base SVG canvas;
 my $svg = SVG->new(id => 'canvas', width => $pixels, height => $pixels);
 my $image = $svg->group(id=>'image');
 
 # Outer loop, need to figure out how to input description
-for(my $i = 0; $i<5; $i++) {
-$p = (1+$i/2)* $r;
-$phi = $i * pi/10;
+for(my $i = 0; $i<4; $i++) {
+$p = (1.4-$i/7)* $r;
+# $phi = $i * pi/10;
 
 # loop 
 my @xs;
@@ -52,6 +74,6 @@ for(my $t=0; $t<(2 * pi * $M); $t = $t + $step) {
 
 # convert array of points to svg polygon
 my $points = $image->get_path(x=>\@xs, y=> \@ys, -type=>'polygon');
-my $c = $image->polygon(transform=>"translate(256, 256)", style=>"stroke: $pen_color; fill: none; stroke-width: $stroke_width", %$points ); 
+my $c = $image->polygon(transform=>"translate(256, 256)", style=>"stroke: $pen_color; fill: $fill_color; stroke-width: $stroke_width", %$points ); 
 }
 print $svg->xmlify;
