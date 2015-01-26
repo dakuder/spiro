6b1083ab740ecb524a754de8af6b3cc094a7d340
dkuder <d@localhost>
2015-01-26 06:28:46 -0800

    Move to CSV input. Lots of debug printfs

--- perl-svg.pl
+++ perl-svg.pl
@@ -36,29 +36,6 @@ my $theta;
 my $offset_x;
 my $offset_y;
 
-# Hardcoded parameters
-$NN=11;
-$MM=5;
-# assuming N>M and N/2 > M;
-my $f = gcd($NN, $MM);
-my $N = $NN / $f;
-my $M = $MM / $f;
-$r = ($M / $N)*$R;
-$p = 1 * $r;
-$phi = pi/20;
-$A = 150;
-$step=(2 * pi)/$A;
-my $sigma=($R-$r)/$r;
-$stroke_width = 1;
-$pen_color = "#000000";
-$fill_color = "#ccff4f";
-$fill_opacity = 1;
-$scale_x = 1;
-$scale_y = 1;
-$theta = 0;
-$offset_x = 0;
-$offset_y = 0;
-
 # create base SVG canvas;
 my $svg = SVG->new(id => 'canvas', width => $pixels, height => $pixels);
 my $image = $svg->group(id=>'image');
@@ -67,7 +44,45 @@ my $image = $svg->group(id=>'image');
 while(<>) {
     chomp;
     my @params = split(',');
-
+    $R = $params[0];
+print STDERR "R = $R\n";
+    $NN = $params[1];
+    $MM = $params[2];
+# assuming N>M and N/2 > M;
+    my $f = gcd($NN, $MM);
+print STDERR "f = $f\n";
+    my $N = $NN / $f;
+    my $M = $MM / $f;
+print STDERR "N = $N; M = $M\n";
+    $r = ($M / $N)*$R;
+print STDERR "r = $r\n";
+    my $sigma=($R-$r)/$r;
+    $p = $params[3];
+    $p = $p * $r;
+print STDERR "p=$p\n";
+    $phi = $params[4];
+print STDERR "phi = $phi\n";
+    $A = $params[5];
+print STDERR "A=$A\n";
+    $step=(2 * pi)/$A;
+    $stroke_width = $params[6];
+print STDERR "stroke_width = $stroke_width\n";
+    $pen_color = $params[7];
+print STDERR "pen_color = $pen_color\n";
+    $fill_color = $params[8];
+print STDERR "fill_color = $fill_color\n";
+    $fill_opacity = $params[9];
+print STDERR "fill_opacity = $fill_opacity\n";
+    $scale_x = $params[10];
+print STDERR "scale_x = $scale_x\n";
+    $scale_y = $params[11];
+print STDERR "scaly_y = $scale_y\n";
+    $theta = $params[12] * pi;
+print STDERR "theta = $theta\n";
+    $offset_x = $params[13];
+    $offset_y = $params[14];
+print STDERR "offset x,y = $offset_x, $offset_y\n";
+    
 # inner loop drawing a single hypocycloid or whater
     my @xs;
     my @ys;
